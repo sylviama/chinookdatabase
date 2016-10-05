f54dbd3b3a57cb89b52edada8aaa74fd65508515
Sylvia Ma <xiangwma@umich.edu>
2016-10-05 11:22:33 -0500

    first

---
+++ chinookdatabase/Model/ChinookContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace BasicEntityFrameworkDataAccess.Models
+{
+    public class ChinookContext : DbContext
+    {
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist>()
+                .ToTable("Artist")
+                .HasKey(c => c.ArtistId);
+
+
+            modelBuilder.Entity<Genre>()
+                .ToTable("Genre")
+                .HasKey(c => c.GenreId);
+        }
+
+
+    }
+}
---
+++ chinookdatabase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicEntityFrameworkDataAccess.Models;
+using System.Data.Entity;
+
+namespace chinookdatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer<ChinookContext>(null);
+
+            ChinookContext dbContext = new ChinookContext();
+            //var artistSearch = "Accept";
+            var artists = dbContext.Artist.Where(a => a.ArtistId==2);
+            foreach (var artist in artists)
+            {
+                Console.WriteLine(artist.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
